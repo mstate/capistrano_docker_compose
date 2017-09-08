a4f49dde404a9ade5508bf0c87aac3a3bdce3a9f
mike <mstate@gmail.com>
2017-09-08 14:01:32 -0400

    testing path again

--- lib/capistrano_docker_compose/railtie.rb
+++ lib/capistrano_docker_compose/railtie.rb
@@ -1,6 +1,6 @@
 class CapistranoDockerCompose::Railtie < Rails::Railtie
   rake_tasks do
     puts "pwd: #{Dir.pwd}"
-    load '../capistrano/tasks/docker_compose.rake'
+    load 'capistrano/tasks/docker_compose.rake'
   end
 end
