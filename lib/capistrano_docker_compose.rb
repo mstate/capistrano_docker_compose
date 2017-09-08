d665f3f3194b4c02b2c86c3227318e74ed6db41a
mike <mstate@gmail.com>
2017-09-07 20:50:38 -0400

    change relative path

--- lib/capistrano_docker_compose.rb
+++ lib/capistrano_docker_compose.rb
@@ -1,3 +1,3 @@
 require_relative 'capistrano_docker_compose/version'
-require_relative './merge_docker_hash'
+require_relative 'merge_docker_hash'
 load File.expand_path("../tasks/version.rake", __FILE__)
